737f0a4ee5e5cc682fb2e87da5c083253f7c5dd2
Krishna1808 <141251951+Krishna1808@users.noreply.github.com>
2026-02-13 22:36:04 +0530

    Add files via upload

---
+++ Assign2.java
@@ -0,0 +1,53 @@
+class Employee{
+    protected double salary;
+
+    Employee(double salary){
+        this.salary=salary;
+    }
+    void displaySalary(){
+        System.out.println("Salary : "+salary);
+    }
+}
+
+class FullTimeEmployee extends Employee{
+
+    FullTimeEmployee(double salary){
+        super(salary);
+    }
+    void calculateSalary(){
+        salary=salary+(salary*0.5);
+    }
+}
+
+class InternEmployee extends Employee{
+
+    InternEmployee(double salary){
+        super(salary);
+    }
+    void calculateSalary(){
+        salary=salary+(salary*.25);
+    }
+}
+
+public class Assign2 {
+    public static void main(String[] args){
+        FullTimeEmployee fte=new FullTimeEmployee(40000);
+        System.out.println("Full time employee before hike : ");
+        fte.displaySalary();
+        fte.calculateSalary();
+        System.out.println("Full time employee salary after hike : ");
+        fte.displaySalary();
+
+        System.out.println();
+
+        InternEmployee intern=new InternEmployee(20000);
+        System.out.println("Intern employee before hike : ");
+        intern.displaySalary();
+        intern.calculateSalary();
+        System.out.println("Intern employee salary after hike : ");
+        intern.displaySalary();
+    }
+}
+
+
+
